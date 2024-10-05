ba45910916d97eeded60f3946be4766dd9d0ce9c
Zegapaincreator <goohagis2@gmail.com>
2024-10-05 23:39:55 +0700

    module3,2.py

--- d.py
+++ d.py
@@ -1,5 +1,5 @@
 def send_email(recipient, message, *, sender="university.help@gmail.com"):
-    if "@" in recipient or not ():
+    if "@" in recipient or (recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
         if sender != recipient:
             if sender == "university.help@gmail.com":
                 print(f"Письмо успешно отправлено с адресса {sender} на адресс {recipient}")
